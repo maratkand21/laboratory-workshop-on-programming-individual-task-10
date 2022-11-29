d73738118b82efa81c147c4511b7fb3e7d808fd8
maratkand21 <109297617+maratkand21@users.noreply.github.com>
2022-11-29 17:14:43 +0800

    Add files via upload

---
+++ Лабораторная работа 10/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.trackBar3 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(364, 349);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 37);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Квадрат";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.square_button_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(364, 401);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(128, 37);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Круг";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.circle_button_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(522, 349);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 37);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Треугольник";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.triangle_button_click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(522, 401);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(150, 37);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Многоугольник";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.poligon_button_click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton1.Location = new System.Drawing.Point(31, 349);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(90, 24);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.Text = "Пунктир";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(26, 320);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 26);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Режим:";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Checked = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton2.Location = new System.Drawing.Point(31, 379);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(131, 24);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Заполненный";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton3.Location = new System.Drawing.Point(31, 409);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(106, 24);
+            this.radioButton3.TabIndex = 7;
+            this.radioButton3.Text = "Сплошной";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(211, 320);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 26);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Цвет:";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 20;
+            this.listBox1.Items.AddRange(new object[] {
+            "Красный",
+            "Синий",
+            "Оранжевый",
+            "Желтый",
+            "Черный",
+            "Розовый",
+            "Белый",
+            "Зеленый"});
+            this.listBox1.Location = new System.Drawing.Point(185, 349);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 84);
+            this.listBox1.TabIndex = 12;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.select_color);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(559, 242);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar1.Size = new System.Drawing.Size(45, 104);
+            this.trackBar1.TabIndex = 13;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.Both;
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(610, 242);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar2.Size = new System.Drawing.Size(45, 104);
+            this.trackBar2.TabIndex = 14;
+            this.trackBar2.TickStyle = System.Windows.Forms.TickStyle.Both;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(558, 226);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 13);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "Ширина";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(610, 226);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Высота";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(656, 226);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 13);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Толщина";
+            // 
+            // trackBar3
+            // 
+            this.trackBar3.Location = new System.Drawing.Point(661, 242);
+            this.trackBar3.Maximum = 20;
+            this.trackBar3.Name = "trackBar3";
+            this.trackBar3.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.trackBar3.Size = new System.Drawing.Size(45, 104);
+            this.trackBar3.TabIndex = 17;
+            this.trackBar3.TickStyle = System.Windows.Forms.TickStyle.Both;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(708, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.trackBar3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Рисунки";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TrackBar trackBar3;
+    }
+}
+
---
+++ Лабораторная работа 10/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public Graphics g;
+        static Color colorFigure = Color.Green;
+        Pen p = new Pen(colorFigure);
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        { 
+            g = CreateGraphics();
+        }
+
+        private void square_button_Click(object sender, EventArgs e)
+        {
+            Refresh();
+
+            p.Color = colorFigure;
+            p.Width = trackBar3.Value;
+
+            float widthFigure = trackBar1.Value + 1;
+            float heightFigure = trackBar2.Value + 1;
+
+            if (radioButton1.Checked)
+            {
+                p.DashStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+                g.DrawRectangle(p, Width / 2 - 100, 50, 200 / widthFigure, 200 / heightFigure);
+            }
+            else if (radioButton2.Checked)
+            {
+                g.FillRectangle(new SolidBrush(colorFigure), new Rectangle(Width / 2 - 100, 50, (int)(200 / widthFigure), (int)(200 / heightFigure)));
+            }
+            else if (radioButton3.Checked)
+            {
+                p.DashStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+                g.DrawRectangle(p, Width / 2 - 100, 50, 200 / widthFigure, 200 / heightFigure);
+            }
+        }
+
+        private void circle_button_Click(object sender, EventArgs e)
+        {
+            Refresh();
+
+            p.Color = colorFigure;
+            p.Width = trackBar3.Value;
+
+            float widthFigure = trackBar1.Value + 1;
+            float heightFigure = trackBar2.Value + 1;
+
+            if (radioButton1.Checked)
+            {
+                p.DashStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+                g.DrawEllipse(p, Width / 2 - 100, 50, 200 / widthFigure, 200 / heightFigure);
+            }
+            else if (radioButton2.Checked)
+            {
+                g.FillEllipse(new SolidBrush(colorFigure), new Rectangle(Width / 2 - 100, 50, (int)(200 / widthFigure), (int)(200 / heightFigure)));
+            }
+            else if (radioButton3.Checked)
+            {
+                p.DashStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+                g.DrawEllipse(p, Width / 2 - 100, 50, 200 / widthFigure, 200 / heightFigure);
+            }
+        }
+
+        private void select_color(object sender, EventArgs e)
+        {
+            string selectedColor = listBox1.SelectedItem.ToString();
+
+            switch(selectedColor)
+            {
+                case ("Красный"):
+                    colorFigure = Color.Red;
+                    break;
+                case ("Синий"):
+                    colorFigure = Color.Blue;
+                    break;
+                case ("Оранжевый"):
+                    colorFigure = Color.Orange;
+                    break;
+                case ("Желтый"):
+                    colorFigure = Color.Yellow;
+                    break;
+                case ("Черный"):
+                    colorFigure = Color.Black;
+                    break;
+                case ("Розовый"):
+                    colorFigure = Color.Pink;
+                    break;
+                case ("Белый"):
+                    colorFigure = Color.White;
+                    break;
+                case ("Зеленый"):
+                    colorFigure = Color.Green;
+                    break;
+            }
+        }
+
+        private void triangle_button_click(object sender, EventArgs e)
+        {
+            Refresh();
+
+            p.Color = colorFigure;
+            p.Width = trackBar3.Value;
+
+            float widthFigure = trackBar1.Value + 1;
+            float heightFigure = trackBar2.Value + 1;
+
+            PointF[] cords =
+             {
+                 new PointF(250 + ((widthFigure > 2) ? widthFigure * 10 : 1),  250 - ((heightFigure > 2) ? heightFigure * 10 : 1)),
+                 new PointF(350, 50),
+                 new PointF(450 - ((widthFigure > 2) ? widthFigure * 10 : 1),  250 - ((heightFigure > 2) ? heightFigure * 10 : 1)),
+                 new PointF(250 + ((widthFigure > 2) ? widthFigure * 10 : 1),  250 - ((heightFigure > 2) ? heightFigure * 10 : 1))
+            };
+
+            if (radioButton1.Checked)
+            {
+                p.DashStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+                g.DrawLines(p, cords);
+            }
+            else if (radioButton2.Checked)
+            {
+                g.FillPolygon(new SolidBrush(colorFigure), cords);
+            }
+            else if (radioButton3.Checked)
+            {
+                p.DashStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+                g.DrawLines(p, cords);
+            }
+        }
+
+        private void poligon_button_click(object sender, EventArgs e)
+        {
+            Refresh();
+
+            p.Color = colorFigure;
+            p.Width = trackBar3.Value;
+
+            float widthFigure = trackBar1.Value + 1;
+            float heightFigure = trackBar2.Value + 1;
+        
+            PointF[] cords = {
+                 new PointF(275 + ((widthFigure > 2) ? widthFigure * 6 : 1),  250 - ((heightFigure > 2) ? heightFigure * 6 : 1)),
+                 new PointF(175 + ((widthFigure > 2) ? widthFigure * 6 : 1), 150),
+                 new PointF(275 + ((widthFigure > 2) ? widthFigure * 6 : 1), 50 + ((heightFigure > 2) ? heightFigure * 6 : 1)),
+                 new PointF(425 - ((widthFigure > 2) ? widthFigure * 6 : 1), 50 + ((heightFigure > 2) ? heightFigure * 6 : 1)),
+                 new PointF(525 - ((widthFigure > 2) ? widthFigure * 6 : 1), 150),
+                 new PointF(425 - ((widthFigure > 2) ? widthFigure * 6 : 1), 250 - ((heightFigure > 2) ? heightFigure * 6 : 1)),
+             };
+
+            if (radioButton1.Checked)
+            {
+                p.DashStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+                g.DrawPolygon(p, cords);
+            }
+            else if (radioButton2.Checked)
+            {
+                g.FillPolygon(new SolidBrush(colorFigure), cords);
+            }
+            else if (radioButton3.Checked)
+            {
+                p.DashStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+                g.DrawPolygon(p, cords);
+            }
+        }
+    }
+}
